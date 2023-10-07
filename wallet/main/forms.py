53634f5b2e2546ebee51a07b5a2a5a5ba7a87eee
Ашот Свазян <ashot.svazyan222@gmail.com>
2023-10-07 23:44:02 +0300

    update

---
+++ wallet/main/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Users
+
+
+class RegisterForm(forms.Form, forms.ModelForm):
+    email = forms.CharField(label="Email", required=True,
+                            widget=forms.TextInput(attrs={"class": "popup__label-input"}))
+    name = forms.CharField(label="Имя", required=True, min_length=4,
+                           widget=forms.TextInput(attrs={"class": "popup__label-input"}))
+    password = forms.CharField(label="Пароль", required=True, min_length=8,
+                               widget=forms.TextInput(attrs={"class": "popup__label-input"}))
+    '''lost = forms.CharField(label="Подтверждение пароля", required=True,
+                           widget=forms.TextInput(attrs={"class": "popup__label-input"}))'''
+
+    class Meta:
+        model = Users
+        fields = ['email', 'name', 'password']
--- wallet/main/views.py
+++ wallet/main/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Assets, Transactions
+from .forms import RegisterForm
+
+
+def registration(request):
+    error = ''
+
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            redirect('wallet')
+        else:
+            error = "Form not valid"
+
+    form = RegisterForm()
+    return render(request, 'main/registration.html', {
+        'form': form,
+        'error': error
+    })
 
 
 def index(request):
@@ -9,34 +28,36 @@ def index(request):
 def wallet(request):
     assets = Assets.objects.all()
     transactions = Transactions.objects.all()
-    return render(request, 'main/wallet.html', {
+    return render(request, 'main/layout/wallet.html', {
         'assets': assets,
         'transactions': transactions,
     })
 
 
 def profile(request):
-    return render(request, 'main/profile.html')
+    return render(request, 'main/layout/profile.html')
 
 
 def analyst(request):
-    return render(request, 'main/analyst.html')
+    return render(request, 'main/layout/analyst.html')
 
 
 def change(request):
-    return render(request, 'main/change.html')
+    return render(request, 'main/layout/change.html')
 
 
-def deposit(request):
-    return render(request, 'main/deposit.html')
+def deposit(request, id):
+    return render(request, 'main/layout/deposit.html', {
+        'id': id
+    })
 
 
 def referral(request):
-    return render(request, 'main/referral.html')
+    return render(request, 'main/layout/referral.html')
 
 
 def achievement(request):
-    return render(request, 'main/achievement.html')
+    return render(request, 'main/layout/achievement.html')
 
 
 def auth(request):
@@ -46,6 +67,3 @@ def auth(request):
 def recall(request):
     return render(request, 'main/recall.html')
 
-
-def registration(request):
-    return render(request, 'main/registration.html')
